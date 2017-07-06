141092cba79bd5caf0e3efa67e713079a3e15e24
Ed Monteiro <edemirmonteir@gmail.com>
2017-07-06 17:13:37 +0100

    Final version

---
+++ README.md
@@ -0,0 +1,4 @@
+# WeatherScraper
+
+    Deployed to Heroku at: https://nameless-mountain-48408.herokuapp.com
+A Weather app created fetching data from API - https://openweathermap.org/
--- index.php
+++ index.php
@@ -3,47 +3,28 @@
 
 	$weather = "";
 	$error = "";
-	
-	if (array_key_exists('city', $_GET)) {
-		
-		$city = str_replace(' ', '', $_GET['city']);
-		
-		$file_headers = @get_headers("http://www.weather-forecast.com/locations/".$city."/forecasts/latest");
-		
-		
-		if($file_headers[0] == 'HTTP/1.1 404 Not Found') {
-	
-			$error = "That city could not be found.";
 
-		} else {
-		
-		$forecastPage = file_get_contents("http://www.weather-forecast.com/locations/".$city."/forecasts/latest");
-		
-		$pageArray = explode('3 Day Weather Forecast Summary:</b><span class="read-more-small"><span class="read-more-content"> <span class="phrase">', $forecastPage);
-			
-		if (sizeof($pageArray) > 1) {
-		
-			$secondPageArray = explode('</span></span></span>', $pageArray[1]);
-			
-			if (sizeof($secondPageArray) > 1) {
+	if ($_GET['city']) {
+        
+		$urlContents = file_get_contents("http://samples.openweathermap.org/data/2.5/weather?q=".$_GET['city'].",uk&appid=68a3bf2dab736a3631c4d3e9682d957b");
+        
+        $weatherArray = json_decode($urlContents, true);
+        
+        if ($weatherArray['cod'] == 200) {
+        
+            $weather = "The weather in ".$_GET['city']." is currently '".$weatherArray['weather'][0]['description']."'. ";
 
-				$weather = $secondPageArray[0];
-					
-			} else {
-			
-				$error = "That city could not be found.";
-					
-			}
-			
-			} else {
-			
-				$error = "That city could not be found.";
-			
-			}
-		
-		}
-		
-	}
+            $tempInCelcius = intval($weatherArray['main']['temp'] - 273);
+
+            $weather .= " The temperature is ".$tempInCelcius."&deg;C and the wind speed is ".$weatherArray['wind']['speed']."m/s.";
+            
+        } else {
+            
+            $error = "Could not find city - please try again.";
+            
+        }
+        
+    }
 
 
 ?>
